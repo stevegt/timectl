7aa474fa9a4dbf879b45acca9e8edc83cffd8903
Steve Traugott <stevegt@t7a.org>
2024-04-10 20:55:25 -0700

    WIP tree_test.go

--- tree_test.go
+++ tree_test.go
@@ -17,7 +17,6 @@ func TestTree(t *testing.T) {
 	end1, err := time.Parse("2006-01-02T15:04:05", "2024-01-01T11:00:00")
 	Ck(err)
 	interval1 := NewInterval(start1, end1)
-	// Insert expects an IntervalT interface
 	tree.Insert(interval1)
 
 	start2, err := time.Parse("2006-01-02T15:04:05", "2024-01-01T10:30:00")
@@ -25,9 +24,33 @@ func TestTree(t *testing.T) {
 	end2, err := time.Parse("2006-01-02T15:04:05", "2024-01-01T11:30:00")
 	Ck(err)
 	interval2 := NewInterval(start2, end2)
-	// Conflicts expects an IntervalT interface and returns a slice of IntervalT interfaces
+	// Conflicts returns a slice of Intervals
 	intervals := tree.Conflicts(interval2)
 	Tassert(t, len(intervals) == 1, fmt.Sprintf("Expected 1 conflict, got %d", len(intervals)))
 	Tassert(t, intervals[0].Start().Equal(start1), fmt.Sprintf("Expected start1, got %v", intervals[0].Start()))
 	Tassert(t, intervals[0].End().Equal(end1), fmt.Sprintf("Expected end1, got %v", intervals[0].End()))
+}
+
+// Ensure that the tree is actually a tree, with a root node and children
+func TestTreeStructure(t *testing.T) {
+	tree := NewTree()
+	start1, err := time.Parse("2006-01-02T15:04:05", "2024-01-01T10:00:00")
+	Ck(err)
+	end1, err := time.Parse("2006-01-02T15:04:05", "2024-01-01T11:00:00")
+	Ck(err)
+	interval1 := NewInterval(start1, end1)
+	tree.Insert(interval1)
+
+	start2, err := time.Parse("2006-01-02T15:04:05", "2024-01-01T10:30:00")
+	Ck(err)
+	end2, err := time.Parse("2006-01-02T15:04:05", "2024-01-01T11:30:00")
+	Ck(err)
+	interval2 := NewInterval(start2, end2)
+	tree.Insert(interval2)
+
+	Tassert(t, tree.Root().Left() != nil, "Expected left child")
+	Tassert(t, tree.Root().Right() != nil, "Expected right child")
+
+	Tassert(t, tree.Root().Left().Interval().Start().Equal(start1), fmt.Sprintf("Expected start1, got %v", tree.Root().Left().Interval().Start()))
+	Tassert(t, tree.Root().Right().Interval().Start().Equal(start2), fmt.Sprintf("Expected start2, got %v", tree.Root().Right().Interval().Start()))
 }
