7c7ff45ec073ebd6d08ce14f9840336f7c485c52
Steve Traugott <stevegt@t7a.org>
2024-04-10 19:27:50 -0700

    WIP TestInterval

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module interval
 
 go 1.21
+
+require github.com/stevegt/goadapt v0.7.0
--- interval_test.go
+++ interval_test.go
@@ -15,8 +15,10 @@ func TestInterval(t *testing.T) {
 	// Interval is a type that represents time interval with a start
 	// and end time.  The start time is inclusive and the end time is
 	// exclusive.  The interval is represented as [start, end).
-	start := time.Parse("2006-01-02T15:04:05", "2024-01-01T10:00:00")
-	end := time.Parse("2006-01-02T15:04:05", "2024-01-01T11:00:00")
+	start, err := time.Parse("2006-01-02T15:04:05", "2024-01-01T10:00:00")
+	Ck(err)
+	end, err := time.Parse("2006-01-02T15:04:05", "2024-01-01T11:00:00")
+	Ck(err)
 	interval := NewInterval(start, end)
 	Tassert(interval.Start() == start, "start time: expected %v, got %v", start, interval.Start())
 	Tassert(interval.End() == end, "end time: expected %v, got %v", end, interval.End())
