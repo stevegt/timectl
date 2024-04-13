17423142854b265bdf119568e3f1395d872ebc0d
Steve Traugott <stevegt@t7a.org>
2024-04-13 12:55:35 -0700

    WIP tree_test.go

--- tree_test.go
+++ tree_test.go
@@ -112,34 +112,18 @@ func TestTreeStructure(t *testing.T) {
 	// tree should span tree.left and tree.right
 	err = expect(tree, "", "2024-01-01T09:00:00", "2024-01-01T13:00:00")
 
-	/*
-		// insert an interval between the root and the right child -- this should
-		// cause the right child to move to tree.right.right and the new interval
-		// to insert at tree.right.left.  The root interval will not
-		// change, and tree.right will be replaced with a new interval that spans
-		// tree.right.left and tree.right.right.
-		start3, err := time.Parse("2006-01-02T15:04:05", "2024-01-01T11:00:00")
-		Ck(err)
-		end3, err := time.Parse("2006-01-02T15:04:05", "2024-01-01T11:30:00")
-		Ck(err)
-		interval3 := NewInterval(start3, end3)
-		tree.Insert(interval3)
-		Tassert(t, tree.interval != nil, "Expected root interval")
-		Tassert(t, tree.left != nil, "Expected left child")
-		Tassert(t, tree.right != nil, "Expected right child")
-		Tassert(t, tree.right.left != nil, "Expected right.left child")
-		Tassert(t, tree.right.right != nil, "Expected right.right child")
-		Tassert(t, tree.interval.Start().Equal(start1), fmt.Sprintf("Expected start1, got %v", tree.interval.Start()))
-		Tassert(t, tree.interval.End().Equal(end2), fmt.Sprintf("Expected end2, got %v", tree.interval.End()))
-		Tassert(t, tree.left.interval.Start().Equal(start1), fmt.Sprintf("Expected start1, got %v", tree.left.interval.Start()))
-		Tassert(t, tree.right.interval.Start().Equal(start3), fmt.Sprintf("Expected start3, got %v", tree.right.interval.Start()))
-		Tassert(t, tree.right.interval.End().Equal(end2), fmt.Sprintf("Expected end2, got %v", tree.right.interval.End()))
-		Tassert(t, tree.right.left.interval.Start().Equal(start3), fmt.Sprintf("Expected start3, got %v", tree.right.left.interval.Start()))
-		Tassert(t, tree.right.left.interval.End().Equal(end3), fmt.Sprintf("Expected end3, got %v", tree.right.left.interval.End()))
-		Tassert(t, tree.right.right.interval.Start().Equal(start2), fmt.Sprintf("Expected start2, got %v", tree.right.right.interval.Start()))
-		Tassert(t, tree.right.right.interval.End().Equal(end2), fmt.Sprintf("Expected end2, got %v", tree.right.right.interval.End()))
-	*/
-
+	// insert an interval between the root and the right child -- this
+	// should cause the new interval to insert at tree.right.left
+	err = insertExpect(tree, "rl", "2024-01-01T11:30:00", "2024-01-01T12:00:00")
+	Tassert(t, err == nil, err)
+	// tree.right should move to tree.right.right
+	err = expect(tree, "rr", "2024-01-01T12:00:00", "2024-01-01T13:00:00")
+	Tassert(t, err == nil, err)
+	// tree.right should span tree.right.left and tree.right.right
+	err = expect(tree, "r", "2024-01-01T11:30:00", "2024-01-01T13:00:00")
+	Tassert(t, err == nil, err)
+	// tree should span tree.left and tree.right
+	err = expect(tree, "", "2024-01-01T09:00:00", "2024-01-01T13:00:00")
 }
 
 func TestConflicts(t *testing.T) {
