f65501249fe84c75d62d39f479a0ab55d1c3c297
Steve Traugott <stevegt@t7a.org>
2024-04-23 03:39:17 -0700

    add easier FindFreePriority test

--- v1/tree_test.go
+++ v1/tree_test.go
@@ -506,12 +506,28 @@ func TestFindFreePriority(t *testing.T) {
 
 	dump(tree, "")
 
+	// find intervals for a 60 minute duration and priority 3 near the
+	// start time.  because priority 3 is higher than the priority of
+	// the busy interval at 9:00, FindFreePriority should return the
+	// priority 2 interval from 9:00 to 9:30 followed by the free
+	// interval from 9:30 to 10:00.
+	intervals := tree.FindFreePriority(true, searchStart, searchEnd, 60*time.Minute, 2)
+	for _, interval := range intervals {
+		t.Logf("%v", interval)
+	}
+	Tassert(t, len(intervals) == 2, "Expected 2 intervals, got %d", len(intervals))
+	err = match(intervals[0], "2024-01-01T09:00:00Z", "2024-01-01T09:30:00Z", 2)
+	Tassert(t, err == nil, err)
+	err = match(intervals[1], "2024-01-01T9:30:00Z", "2024-01-01T10:00:00Z", 0)
+	Tassert(t, err == nil, err)
+	Tassert(t, intervals[1] == i1000_1100, "Expected %v, got %v", i1000_1100, intervals[1])
+
 	// find intervals for a 60 minute duration and priority 2 near the
 	// start time.  because priority 2 is not higher than the priority of
 	// the busy interval at 9:00, FindFreePriority should return the free
 	// interval from 9:30 to 10:00 followed by the priotity 1 interval
 	// from 10:00 to 11:00.
-	intervals := tree.FindFreePriority(true, searchStart, searchEnd, 60*time.Minute, 2)
+	intervals = tree.FindFreePriority(true, searchStart, searchEnd, 60*time.Minute, 2)
 	for _, interval := range intervals {
 		t.Logf("%v", interval)
 	}
